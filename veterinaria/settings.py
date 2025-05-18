ca903b44ea821399eea1fa0ec688723007430520
Angel Hernández <119709720+amewir@users.noreply.github.com>
2025-05-18 02:34:22 -0600

    Update settings.py

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -16,7 +16,11 @@ import base64
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
